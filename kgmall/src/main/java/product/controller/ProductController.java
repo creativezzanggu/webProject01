77004884cae6e8a4ed36616b97f55fad4c94df11
Your Name Here <heahn153@gmail.com>
2019-06-10 15:19:07 +0900

    cartcontroll, productcontroll

--- kgmall/src/main/java/product/controller/ProductController.java
+++ kgmall/src/main/java/product/controller/ProductController.java
@@ -2,10 +2,8 @@ package product.controller;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -54,7 +52,8 @@ public class ProductController {
 	@RequestMapping(value="/getColor.do", method=RequestMethod.POST)
 	public ModelAndView getColor(@RequestParam String name) {
 		ModelAndView mav = new ModelAndView();
-		
+		name = name.toUpperCase();
+		System.out.println(name);
 		List<String> list = productDAO.getColor(name);
 		mav.addObject("list", list);
 		mav.setViewName("jsonView");
@@ -83,18 +82,22 @@ public class ProductController {
 	public ModelAndView selectCookie(HttpServletResponse response,HttpServletRequest request){
 		ModelAndView mav = new ModelAndView();
 		Map<String,String> map= new HashMap<String, String>();
-		
+		String str[];
 		Cookie[] getCookie = request.getCookies();
 		if(getCookie != null){	
-			for(int i=0; i<getCookie.length-3; i++){
+			for(int i=0; i<getCookie.length; i++){
 				Cookie c = getCookie[i];
-				String name1 = c.getName(); // 쿠키 이름 가져오기
-				String value = c.getValue(); // 쿠키 값 가져오기
-				map.put(name1, value);
-				
+				str = c.getName().split("_");
+				try {
+					if(str[2]!=""){
+						String name1 = c.getName(); // 쿠키 이름 가져오기
+						String value = c.getValue(); // 쿠키 값 가져오기
+						map.put(name1, value);
+					}
+				}catch(ArrayIndexOutOfBoundsException e) {
+					//e.printStackTrace();
+				}
 			}
-			for( String key : map.keySet() ){
-	        }
 
 		}		
 		mav.addObject("map", map);
@@ -120,18 +123,25 @@ public class ProductController {
 	@RequestMapping(value="/insertCookie.do",method=RequestMethod.POST)
 	public ModelAndView insertCookie(@RequestParam String id,HttpServletResponse response,HttpServletRequest request){
 		ModelAndView mav = new ModelAndView();
+		String str[];
 		List<CartDTO> list= new ArrayList<CartDTO>();
 		Cookie[] getCookie = request.getCookies();
 		if(getCookie != null){	
-			for(int i=0; i<getCookie.length-3; i++){
+			for(int i=0; i<getCookie.length; i++){
 				CartDTO cartDTO = new CartDTO();
 				Cookie c = getCookie[i];
-				cartDTO.setProduct(c.getName()); // 쿠키 이름 가져오기
-				if(c.getValue().length()<10) {
-					cartDTO.setProductCount(Integer.parseInt(c.getValue())); // 쿠키 값 가져오기
+				str = c.getName().split("_");
+				try {
+					if(str[2]!=""){
+						System.out.println("sdf"+str[2]);
+						cartDTO.setProduct(c.getName()); // 쿠키 이름 가져오기
+						cartDTO.setProductCount(Integer.parseInt(c.getValue())); // 쿠키 값 가져오기
+						cartDTO.setSellId(id);
+						list.add(cartDTO);
+					}
+				}catch(ArrayIndexOutOfBoundsException e) {
+					//e.printStackTrace();
 				}
-				cartDTO.setSellId(id);
-				list.add(cartDTO);
 			}
 		}
 		cartDAO.insertCart(list);
@@ -170,7 +180,7 @@ public class ProductController {
 	@RequestMapping(value="/getCount.do", method=RequestMethod.POST)
 	public ModelAndView getCount(@RequestParam String name) {
 		ModelAndView mav = new ModelAndView();
-		name = name.toLowerCase();
+		name = name.toUpperCase();
 		System.out.println(name);
 		int count = productDAO.getCount(name);
 		mav.addObject("count", count);
